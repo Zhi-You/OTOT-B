7b7117d447154e32f0179fc24e12916fdb5f8309
Zhi-You <tzyou98@gmail.com>
2022-10-05 18:23:09 +0800

    Implement a simple backend for Task B1

---
+++ backend/api-routes.js
@@ -0,0 +1,27 @@
+// Initialize express router
+let router = require('express').Router();
+// Import contact controller
+var contactController = require('./contactController');
+
+// Set default API response
+router.get('/', function (req, res) {
+    res.json({
+        status: 'API Its Working',
+        message: 'Welcome to my CS3219 Task B REST API'
+    });
+});
+
+
+// Contact routes
+router.route('/contacts')
+    .get(contactController.index)
+    .post(contactController.new);
+
+router.route('/contacts/:contact_name')
+    .get(contactController.view)
+    .patch(contactController.update)
+    .put(contactController.update)
+    .delete(contactController.delete);
+
+// Export API routes
+module.exports = router;
---
+++ backend/contactController.js
@@ -0,0 +1,96 @@
+// Import contact model
+Contact = require('./contactModel');
+
+
+// List all contacts
+exports.index = function (req, res) {
+    Contact.get(function (err, contacts) {
+        if (err) {
+            res.json({
+                status: "error",
+                message: err,
+            });
+        }
+        res.json({
+            status: "success",
+            message: "Contacts retrieved successfully",
+            data: contacts
+        });
+    });
+};
+
+
+// Handle create contact actions
+exports.new = function (req, res) {
+    console.log(req.body);
+    var contact = new Contact();
+    contact.name = req.body.name ? req.body.name : contact.name;
+    contact.gender = req.body.gender;
+    contact.email = req.body.email;
+    contact.phone = req.body.phone;
+    
+    // save the contact and check for errors
+    contact.save(function (err) {
+        if (err)
+             res.json(err);
+        else
+        res.json({
+            message: 'New contact created!',
+            data: contact
+        });
+    });
+};
+
+
+// Handle view contact info
+exports.view = function (req, res) {
+    console.log(req.params.contact_name);
+
+    Contact.findOne({ name: req.params.contact_name
+    }, function (err, contact) {
+        if (err)
+            res.send(err);
+        res.json({
+            message: 'Contact details loading..',
+            data: contact
+        });
+    });
+};
+
+
+// Handle update contact info
+exports.update = function (req, res) {
+    Contact.findOne({ name: req.params.contact_name
+    }, function (err, contact) {
+        if (err)
+            res.send(err);
+        contact.name = req.body.name ? req.body.name : contact.name;
+        contact.gender = req.body.gender;
+        contact.email = req.body.email;
+        contact.phone = req.body.phone;
+        // save the contact and check for errors
+        contact.save(function (err) {
+            if (err)
+                res.json(err);
+            res.json({
+                message: 'Contact Info updated',
+                data: contact
+            });
+        });
+    });
+};
+
+
+// Handle delete contact
+exports.delete = function (req, res) {
+    Contact.deleteOne({
+        name: req.params.contact_name
+    }, function (err, contact) {
+        if (err)
+            res.send(err);
+        res.json({
+            status: "success",
+            message: 'Contact deleted'
+        });
+    });
+};
