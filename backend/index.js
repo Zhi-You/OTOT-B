53eaf84ae015dd4b7701c5f8fffa58d9995f4a23
Zhi-You <tzyou98@gmail.com>
2022-10-13 01:37:43 +0800

    Added Serverless capabilities to deploy onto AWS Lambda

--- backend/index.js
+++ backend/index.js
@@ -11,24 +11,40 @@ let apiRoutes = require("./api-routes");
 // Use env files
 require('dotenv').config()
 
+// Using serverless
+const serverless = require("serverless-http");
+
+
+// set up cors
+const cors = require("cors");
+const corsOptions = {
+  origin: "*",
+  credentials: true, 
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions)); 
+
 // Configure bodyparser to handle post requests
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
 
-var mongoDBUrl = 'mongodb://localhost/ContactBook(TaskB)'
+// var mongoDBUrl = 'mongodb://localhost/ContactBook(TaskB)'
 // console.log(process.env.isPROD)
-if (process.env.isPROD == 'True')
-    var mongoDBUrl = process.env.PRODUrl;
+// if (process.env.isPROD == 'True')
+//     var mongoDBUrl = process.env.PRODUrl;
+
+var mongodbUri = 'mongodb+srv://admin:admin@cluster0.pthalgf.mongodb.net/test';
 
 // Connect to Mongoose and set connection variable
-mongoose.connect(mongoDBUrl, 
+mongoose.connect(mongodbUri, 
 { 
-    useNewUrlParser: true
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+
 });
 
-console.log(mongoDBUrl)
 var db = mongoose.connection;
 
 // Added check for DB connection
@@ -46,9 +62,11 @@ app.get('/', (req, res) => res.send('Hello World with Express!'));
 // Use Api routes in the App
 app.use('/api', apiRoutes);
 
-module.exports = app
 
 // Launch app to listen to specified port
 app.listen(port, function () {
     console.log("Running ContactBook on port " + port);
 });
+
+//module.exports = app
+module.exports.handler = serverless(app);
