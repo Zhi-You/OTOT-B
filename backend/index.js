4c98efff117e297a45485f8c12204bf224060ad7
Zhi-You <tzyou98@gmail.com>
2022-10-12 10:50:20 +0800

    Testing CI workflow

--- backend/index.js
+++ backend/index.js
@@ -33,7 +33,7 @@ else
 var port = process.env.PORT || 8080;
 
 // Send message for default URL
-app.get('/', (req, res) => res.send('Hello World with Express'));
+app.get('/', (req, res) => res.send('Hello World with Express!'));
 
 // Use Api routes in the App
 app.use('/api', apiRoutes);
